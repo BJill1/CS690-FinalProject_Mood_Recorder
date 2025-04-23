78d08861c65fea95cf561b12dc5dac45666cd7ea
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-04-23 07:40:23 +0000

    Credentials for existing user added

--- MoodRecorderProject/MoodRecorder/Program.cs
+++ MoodRecorderProject/MoodRecorder/Program.cs
@@ -12,10 +12,11 @@ class Program
         .Title("Existing User or New User?")
         .PageSize(10)
         .AddChoices(new[] {
-            "existing User", "new user"
+            "existing user", "new user"
         }));
     
         string user_password;
+        List<string> ListOfUser_credentials = new List<string>();
         if(user_type == "new user")
         {
             Console.WriteLine("Please enter a user name.");
@@ -33,6 +34,22 @@ class Program
                     user_password = Console.ReadLine();
                 } while(user_password.Replace(" ", "").Length < 6);
                 File.AppendAllText("User_IDs.txt", user_name + ":" + user_password.Replace(" ", "") + Environment.NewLine);
+            }
+        }
+        else if(user_type == "existing user"){
+            Console.WriteLine("Please enter your username: ");
+            string userN = Console.ReadLine();
+            Console.WriteLine("Please enter your password");
+            string userP = Console.ReadLine();
+            string User_credentials = userN + ":" + userP;
+            foreach(string line in File.ReadLines("User_IDs.txt")){
+                ListOfUser_credentials.Add(line);
+            }
+            if(ListOfUser_credentials.Contains(User_credentials)){
+                Console.WriteLine("You entered the correct credentials");
+            }
+            else{
+                Console.WriteLine("Either the password or the username is incorrect");
             }
         }
     }
