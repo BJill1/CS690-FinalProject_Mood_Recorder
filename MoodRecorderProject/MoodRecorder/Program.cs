fdc15fcd55acc89de45c3e95005436f93fa522db
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-04-27 19:21:15 +0000

    Minor change to test automatic testing

--- MoodRecorderProject/MoodRecorder/Program.cs
+++ MoodRecorderProject/MoodRecorder/Program.cs
@@ -9,7 +9,7 @@ class Program
         ProgramUI.show();
 
         string[] Some_happy_words = { "fantastic", "hopeful", "joyful", "relaxed", "cheerful", "delighted", "peaceful" };
-        string[] Some_sad_words = { "lonely", "heartbroken", "miserable", "depressed", "hopeless", "somber", "dejected" };
+        string[] Some_sad_words = { "lonely", "unmotivated", "heartbroken", "miserable", "depressed", "hopeless", "somber", "dejected" };
         string[] Some_happiness_triggers = { "had a good night sleep", "went to a park yesterday", "welcome a grandson", "watching a fun movie", "painting", "hanging out with friends", "went to the gym" };
         string[] Some_sadness_triggers = { "spent much time alone", "lost a loved one", "Look down on myself", "had arguments with some friends", "did not sleep well overnight", "stuck on a project", "having some painful memories", "financially unstable" };
         List<string> HappyWords_to_addList = new List<string>();
