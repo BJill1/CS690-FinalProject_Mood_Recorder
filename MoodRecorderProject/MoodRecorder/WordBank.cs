861967bb572200e9d0d20579d9e1258016a93e42
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-04-27 12:09:37 +0000

    untested changes

--- MoodRecorderProject/MoodRecorder/WordBank.cs
+++ MoodRecorderProject/MoodRecorder/WordBank.cs
@@ -6,6 +6,8 @@ public class WordBank
 {
     List<string> Existing_happy_words = new List<string>();
     List<string> Existing_not_happy_words = new List<string>();
+    List<string> ExistingHappinessTriggers = new List<string>();
+    List<string> ExistingSadnessTriggers = new List<string>();
 
     public WordBank(){
         if(!File.Exists("HappyWords.txt")){
@@ -13,6 +15,12 @@ public class WordBank
         }
         if(!File.Exists("NotHappyWords.txt")){
             File.Create("NotHappyWords.txt");
+        }
+        if(!File.Exists("HappinessTriggers.txt")){
+            File.Create("HappinessTriggers.txt");
+        }
+        if(!File.Exists("SadnessTriggers.txt")){
+            File.Create("SadnessTriggers.txt");
         }
     }
     public void happy_words(List<string> happy_word_list)
@@ -34,6 +42,28 @@ public class WordBank
         foreach(string word in Not_happy_word_list){
             if(!Existing_not_happy_words.Contains(word)){
                 File.AppendAllText("NotHappyWords.txt", word + Environment.NewLine);
+            }
+        }
+    }
+    public void happiness_triggers(List<string> happiness_trigger_list)
+    {
+        foreach(string line in File.ReadLines("HappinessTriggers.txt")){
+            ExistingHappinessTriggers.Add(line);
+        }
+        foreach(string word in happiness_trigger_list){
+            if(!ExistingHappinessTriggers.Contains(word)){
+                File.AppendAllText("HappinessTriggers.txt", word + Environment.NewLine);
+            }
+        }
+    }
+    public void sadness_triggers(List<string> sadness_trigger_list)
+    {
+        foreach(string line in File.ReadLines("SadnessTriggers.txt")){
+            ExistingSadnessTriggers.Add(line);
+        }
+        foreach(string word in sadness_trigger_list){
+            if(!ExistingSadnessTriggers.Contains(word)){
+                File.AppendAllText("SadnessTriggers.txt", word + Environment.NewLine);
             }
         }
     }
